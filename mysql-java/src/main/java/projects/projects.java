81913176a5220363b646c3f2429d0d58cf2f68cd
Grover McSherry <grmcsherry@gmail.com>
2025-06-28 15:13:15 -0600

    Add files via upload

---
+++ mysql-java/src/main/java/projects/projects.java
@@ -0,0 +1,20 @@
+package projects;
+
+import projects.dao.DbConnection;
+
+public class projects {
+
+	public projects() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+		
+			
+			DbConnection.getConnection();
+			
+		}
+	
+
+}
